534e9a58d968cc72a2ea65575ccdad4dc246dcfa
Matlaha Daniil <danyamatlaha63@gmail.com>
2024-12-12 20:11:02 +0200

    new commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const userInfo = document.getElementById('user-info');
 const slot1 = document.getElementById('slot1');
 const slot2 = document.getElementById('slot2');
@@ -10,11 +9,9 @@ const Slots = ['pear', 'strawberry', 'lime', 'cherry', 'grape'];
 setTimeout(beginGame, 300);
 generateButton.disabled = true;
 
-
 let attempts = 0;
 let maxAttempts = 3;
 let userName;
-
 function beginGame(){
     userName = prompt("Введіть своє ім\'я:");
     slot1.src = "Resources/bg.png";
@@ -56,79 +53,7 @@ function spinSlots() {
         setTimeout(restartGame, 2000);
     } else {
         result.textContent = "Спроба " + attempts + " з " + maxAttempts;
-=======
-$(document).ready(function () {
-    const words = [
-      { word: "always", translation: "завжди" },
-      { word: "never", translation: "ніколи" },
-      { word: "see", translation: "бачити" },
-      { word: "goodbye", translation: "прощавай" },
-      { word: "thanks", translation: "дякую" },
-      { word: "please", translation: "будь ласка" },
-      { word: "sorry", translation: "вибач" },
-      { word: "if", translation: "якщо" },
-      { word: "how", translation: "як" },
-      { word: "maybe", translation: "можливо" },
-    ];
-    
-    let currentIndex = 0;
-    let correctCount = 0;
-    let incorrectCount = 0;
-  
-    function shuffle(array) {
-      return array.sort(() => Math.random() - 0.5);
->>>>>>> 25268e5dfe076cbb070d2252bcf571bde64e55f9
     }
-  
-    const shuffledWords = shuffle([...words]);
-  
-    function updateUI() {
-      $("#word").text(shuffledWords[currentIndex].word);
-      $("#translation").val("");
-      $("#progress").text("Крок: " + (currentIndex + 1)  + "/" + words.length);
-      $("#stats").text("Правильно: " + correctCount + " ,  Помилок: " + incorrectCount);
-    }
-  
-    function showModal() {
-      let level;
-      const checkLevel = (correctCount / words.length) * 100;
-  
-      if (checkLevel >= 80) level = "Високий";
-      else if (checkLevel >= 50) level = "Середній";
-      else level = "Початківець";
-  
-      $("#result").text("Ваш рівень: " + level);
-      $("#modal").show();
-    }
-  
-    $("#check").click(function () {
-      const userInput = $("#translation").val().trim().toLowerCase();
-      const correctTranslation = shuffledWords[currentIndex].translation;
-  
-      if (userInput === correctTranslation) {
-        correctCount++;
-      } else {
-        incorrectCount++;
-      }
-  
-      currentIndex++;
-  
-      if (currentIndex < words.length) {
-        updateUI();
-      } else {
-        showModal();
-      }
-    });
-  
-    $("#restart").click(function () {
-      currentIndex = 0;
-      correctCount = 0;
-      incorrectCount = 0;
-  
-      shuffle(shuffledWords);
-      updateUI();
-      $("#modal").hide();
-    });
-    updateUI();
-  });
-  
+}
+
+generateButton.addEventListener('click', spinSlots);
